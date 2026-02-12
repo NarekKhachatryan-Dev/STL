c69cec88834a91b02eb43a4fc3ddec5c2b1d17ff
NaroArmenia <naroarmenia9@gmail.com>
2026-02-12 22:10:25 +0400

    12.02.2026

--- smart_pointers/unique_ptr.hpp
+++ smart_pointers/unique_ptr.hpp
@@ -6,71 +6,71 @@
 namespace My {
 template <typename T, typename Deleter = std::default_delete<T>>
 class unique_ptr {
-    private:
-        T* ptr;
-        Deleter deleter;
+private:
+    T* ptr;
+    Deleter deleter;
 
-    public:
-        explicit unique_ptr(T* p = nullptr, Deleter d = Deleter()) : ptr(p), deleter(d) {}
-        ~unique_ptr() {
+public:
+    explicit unique_ptr(T* p = nullptr, Deleter d = Deleter()) : ptr(p), deleter(d) {}
+    ~unique_ptr() {
+        deleter(ptr);
+    }
+    
+    unique_ptr(unique_ptr&& other) noexcept : ptr(other.ptr), deleter(std::move(other.deleter)) {
+        other.ptr = nullptr;
+    }
+
+    operator bool() const {
+        return ptr != nullptr;
+    }
+
+    unique_ptr& operator=(unique_ptr&& other) noexcept {
+        if(this != &other) {
             deleter(ptr);
-        }
-        
-        unique_ptr(unique_ptr&& other) noexcept : ptr(other.ptr), deleter(std::move(other.deleter)) {
+            ptr = other.ptr;
+            deleter = std::move(other.deleter);
             other.ptr = nullptr;
         }
+        return *this;
+    }
 
-        operator bool() const {
-            return ptr != nullptr;
-        }
+    T& operator*() {
+        return *ptr;
+    }
 
-        unique_ptr& operator=(unique_ptr&& other) noexcept {
-            if(this != &other) {
-                deleter(ptr);
-                ptr = other.ptr;
-                deleter = std::move(other.deleter);
-                other.ptr = nullptr;
-            }
-            return *this;
-        }
+    T* operator->() {
+        return ptr;
+    }
 
-        T& operator*() {
-            return *ptr;
-        }
+    T& operator[] (size_t index) {
+        return ptr[index];
+    }
 
-        T* operator->() {
-            return ptr;
-        }
+    const T& operator*() const {
+        return *ptr;
+    }
 
-        T& operator[] (size_t index) {
-            return ptr[index];
-        }
+    const T* operator->() const {
+        return ptr;
+    }
 
-        const T& operator*() const {
-            return *ptr;
-        }
+    const T& operator[] (size_t index) const {
+        return ptr[index];
+    }
 
-        const T* operator->() const {
-            return ptr;
-        }
+    T* get() const {
+        return ptr;
+    }
 
-        const T& operator[] (size_t index) const {
-            return ptr[index];
-        }
+    void reset(T* p = nullptr) {
+        deleter(ptr);
+        ptr = p;
+    }
 
-        T* get() const {
-            return ptr;
-        }
-
-        void reset(T* p = nullptr) {
-            deleter(ptr);
-            ptr = p;
-        }
-
-        T* release() {
-            T* temp = ptr;
-            ptr = nullptr;
-            return temp;
-        }
+    T* release() {
+        T* temp = ptr;
+        ptr = nullptr;
+        return temp;
+    }
 };
 }
